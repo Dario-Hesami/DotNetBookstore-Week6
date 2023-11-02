27597569b42c1d0d5c4aa1f36e6f4227b22ed839
Dario Hesami <Dario.Hesami@DESKTOP-4SPMNKA>
2023-11-01 22:10:07 -0400

    Ready to work on authentication and authorization

--- DotNetBookstore/Controllers/CategoriesController.cs
+++ DotNetBookstore/Controllers/CategoriesController.cs
@@ -7,11 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DotNetBookstore.Data;
 using DotNetBookstore.Models;
+// *
 using Microsoft.AspNetCore.Authorization;
-
 namespace DotNetBookstore.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -30,7 +30,7 @@ namespace DotNetBookstore.Controllers
         }
 
         // GET: Categories/Details/5
-        [AllowAnonymous]
+        //[AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Categories == null)
